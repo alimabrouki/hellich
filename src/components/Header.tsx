6697b8733653310cd54163e60147a84c7f5bf608
alimab <alimabrouki9797@gmail.com>
2026-02-17 16:31:18 +0100

    add: toggle menu function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react';
 import logo from '../assets/images/logo.svg'
 import NavLink from "./NavLink"
-function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
+
+type HeaderProps = {
+  handleMenuOpen: (state: boolean) => void;
+  menuOpen: boolean;
+}
+
+function Header({handleMenuOpen,menuOpen}: HeaderProps) {
+  
   return (
 
-      <div className="header relative">
+      <div className="header z-20 relative">
           <div className="head sm:bg-head-bg sm:rounded-4xl sm:mx-24 flex items-center justify-end ">
             <div className="head-links max-sm:hidden">
               <NavLink href={"/"}>
@@ -22,7 +27,7 @@ function Header() {
               </NavLink>
             </div>
             <img  className="w-[140px] mr-auto sm:mr-5" src={logo} alt="" />
-            <button onClick={() => setMenuOpen(!menuOpen)} className="menu-button relative sm:hidden py-2 px-3.5 bg-[#eaf9fb] mr-3 rounded-[3px] text-main-bleu ">
+            <button onClick={() => handleMenuOpen(!menuOpen)} className="menu-button relative sm:hidden py-2 px-3.5 bg-[#eaf9fb] mr-3 rounded-[3px] text-main-bleu ">
               <span className="menu-dot rounded-[40%] size-2 absolute translate-x-10 -translate-y-1 bg-main-bleu"></span>
               <span className="menu inline-block -translate-x-2 translate-y-1.25 font-[650]">Menu</span>
               </button>
