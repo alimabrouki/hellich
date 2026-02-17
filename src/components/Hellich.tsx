38d7c3833a0e4f68699ef94fd23f7f516981431c
alimab <alimabrouki9797@gmail.com>
2026-02-17 14:21:50 +0100

    add: overflow hidden when menu is open to prevent scrolling

--- src/components/Hellich.tsx
+++ src/components/Hellich.tsx
@@ -1,15 +1,23 @@
-import profileImage from "./assets/images/hellich-hero-image.png"
+import profileImage from "../assets/images/hellich-hero-image.png"
 import StaticGrainBackground from "../utils/StaticGrainBackground"
 import Header from "./Header"
+import { useEffect, useState } from 'react';
 
 function Hellich() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+ 
+
+  const handleMenuOpen = (state:boolean) => {
+    setMenuOpen(state)
+  }
   return (
     <div className="wrapper">
-      <div className="hellich relative pt-8 overflow-visible bg-black rounded-2xl">
+      <div className={`hellich relative pt-8 overflow-visible bg-black rounded-2xl" ${menuOpen ? 'before:absolute before:w-full before:h-full before:top-0 before:left-0 before:bg-[#eaf9fb] before:z-20 overflow-hidden' : ''}`}>
 
         <StaticGrainBackground className="absolute inset-0 rounded-2xl" />
 
-        <Header />
+        <Header menuOpen={menuOpen} handleMenuOpen={handleMenuOpen} />
 
         <div className="hero relative z-10 min-h-screen flex items-center justify-center">
 
