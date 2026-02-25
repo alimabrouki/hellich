3f174b2e9183ee7e9656b4a4b261b7242d2c2c73
alimab <alimabrouki9797@gmail.com>
2026-02-25 12:01:03 +0100

    refactor: prettify StartBtnMobile component

--- src/components/header/StartBtnMobile.tsx
+++ src/components/header/StartBtnMobile.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router'
-import type { menuOpenProps } from '../../utils/menuOpenProps'
+import type { menuProps } from '../../utils/menuProps'
 
-function StartBtnMobile ({ menuOpen }: menuOpenProps) {
+function StartBtnMobile ({ menuOpen }: menuProps) {
   return (
     <Link
       className={`text-head-btn bg-black self-center rounded-2xl px-9 py-2 flex items-center font-bold
-                  transition-all duration-500 ease-out delay-[400ms]
+                  transition-all duration-500 ease-out delay-400
                   ${
                     menuOpen
                       ? 'translate-y-0 opacity-100'
