05f05dda3de9d02420d533516d322914397acf53
alimab <alimabrouki9797@gmail.com>
2026-02-25 12:00:03 +0100

    refactor: add Logo component

---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,14 @@
+import type { menuProps } from '../../utils/menuProps'
+import logo from '../../assets/images/logo.svg'
+
+function Logo ({ menuOpen }: menuProps) {
+  return (
+    <img
+      className={`w-35 rounded-[3px] mr-auto lg:mr-5 ${menuOpen && 'bg-black'}`}
+      src={logo}
+      alt=''
+    />
+  )
+}
+
+export default Logo
