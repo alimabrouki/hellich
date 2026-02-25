4c36fdaa77657c7204155347c0dabc8e07148196
alimab <alimabrouki9797@gmail.com>
2026-02-25 11:59:46 +0100

    refactor: add HeadLinks component

---
+++ src/components/header/HeadLinks.tsx
@@ -0,0 +1,21 @@
+import NavLink from '../NavLink'
+
+function HeadLinks () {
+  const links = [
+    { href: '/faq', label: 'الأسئلة الشائعة', num: '04' },
+    { href: '/contact', label: 'تواصل معي', num: '02' },
+    { href: '/about', label: 'من أنا ؟', num: '03' },
+    { href: '/program', label: 'برامج', num: '01' }
+  ]
+  return (
+    <div className='hidden lg:flex head-links'>
+      {links.map(link => (
+        <NavLink key={link.href} menuOpen={false} href={link.href}>
+          {link.label}
+        </NavLink>
+      ))}
+    </div>
+  )
+}
+
+export default HeadLinks
