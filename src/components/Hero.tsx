4fe9bca839a064ecc9c49ebe223e8aeda0b668f8
alimab <alimabrouki9797@gmail.com>
2026-02-26 17:14:45 +0100

    add: remove typing text and add sliding title with mix-blend

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 import type { menuProps } from '../utils/menuProps'
 import profileImage from '../assets/images/hellich-hero-image.png'
 import { useEffect, useState } from 'react'
+import { Dot } from 'lucide-react'
 
 function Hero ({ menuOpen }: menuProps) {
-  const fullText = 'تدريب مبني على فهم حقيقي'
-
-  const [displayedText, setDisplayedText] = useState('')
   const [animate, setAnimate] = useState(false)
+
+  // const text = 'تدريب  مبني على فهم حقيقي'
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,34 +16,18 @@ function Hero ({ menuOpen }: menuProps) {
     return () => clearTimeout(timer)
   }, [])
 
-  useEffect(() => {
-    if (!animate) return
-
-    let index = 0
-
-    const typing = setInterval(() => {
-      setDisplayedText(fullText.slice(0, index + 1))
-      index++
-
-      if (index === fullText.length) {
-        clearInterval(typing)
-      }
-    }, 100) // typing speed
-
-    return () => clearInterval(typing)
-  }, [animate])
   return (
     <div
-      className={`hero relative bottom-20 isolate overflow-hidden flex flex-col items-center
-  transition-opacity duration-300
-  ${menuOpen ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
+      className={`hero relative bottom-20 overflow-hidden flex flex-col items-center
+      transition-opacity duration-300
+      ${menuOpen ? 'opacity-0 pointer-events-none' : 'opacity-100'}`}
     >
-      {/* Image */}
+      {/* IMAGE */}
       <div
         className={`
-        w-full flex justify-center pt-10
-        transform transition-all duration-900 ease-out
-        ${animate ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'}
+    relative w-full mb-[333px] flex justify-center pt-10
+    transform transition-all duration-900 ease-out
+    ${animate ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'}
   `}
       >
         <img
@@ -54,45 +38,57 @@ function Hero ({ menuOpen }: menuProps) {
       [mask-image:linear-gradient(to_bottom,black_70%,transparent_100%)]
     '
         />
-      </div>
-      <h1
-        className={`
-            text-center
-            text-4xl sm:text-5xl lg:text-6xl
-            text-pappay
-            font-cairo
-            font-black
-            text-[75px]
-            z-10
-            mb-20
-            transform transition-all duration-700 ease-out delay-300
-            ${
-              animate ? 'translate-y-0 opacity-100' : 'translate-y-16 opacity-0'
-            }
-          `}
-      >
-        {displayedText}
-      </h1>
 
+        {/* Sliding Text Overlay */}
+        <div className='absolute inset-0 flex items-center justify-center overflow-hidden pointer-events-none'>
+          <div
+            className='
+        sliding-text
+        mix-blend-exclusion
+        flex
+        whitespace-nowrap
+        font-cairo
+        font-black
+        text-[80px] sm:text-[120px] lg:text-[160px]
+        text-pappay
+        select-none
+      '
+          >
+            <span className='mx-16'>
+              تدريب
+              <Dot className='inline' strokeWidth={20} />
+              مبني
+              <Dot className='inline' strokeWidth={20} />
+              على
+              <Dot className='inline  ' strokeWidth={20} />
+              فهم
+              <Dot className='inline' strokeWidth={20} />
+              حقيقي
+            </span>
+          </div>
+        </div>
+      </div>
+
+      {/* GRADIENT */}
       <div
         className='
-      pointer-events-none absolute bottom-0 left-0 right-0 z-0
-      h-[64%] lg:h-[45%]
-      bg-gradient-to-t
-      from-[#03a6db]
-      via-[#03a6db]/70
-      to-transparent
-    '
+          pointer-events-none absolute bottom-0 left-0 right-0 z-0
+          h-[64%] lg:h-[45%]
+          bg-gradient-to-t
+          from-[#03a6db]
+          via-[#03a6db]/70
+          to-transparent
+        '
       />
 
       <div
         className='
-      hidden lg:block
-      pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2
-      w-[60%] h-[25%]
-      bg-[radial-gradient(ellipse_at_center,_transparent_40%,_#03a6db_90%)]
-      blur-2xl
-    '
+          hidden lg:block
+          pointer-events-none absolute bottom-0 left-1/2 -translate-x-1/2
+          w-[60%] h-[25%]
+          bg-[radial-gradient(ellipse_at_center,_transparent_40%,_#03a6db_90%)]
+          blur-2xl
+        '
       />
     </div>
   )
