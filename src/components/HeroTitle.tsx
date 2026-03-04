32567dfb13fd64f23da565bcf8cab44f6769e7cc
alimab <alimabrouki9797@gmail.com>
2026-03-04 17:04:12 +0100

    style: adjust hero title line height

--- src/components/HeroTitle.tsx
+++ src/components/HeroTitle.tsx
@@ -2,7 +2,7 @@ function HeroTitle () {
   return (
     <h1
       dir='rtl'
-      className='text-[clamp(4.4rem,19vw,7.9rem)] sm:text-[clamp(4.8rem,12.2vw,11.8rem)] text-second-bg font-extrabold text-right leading-[0.9] tracking-tight'
+      className='text-[clamp(4.4rem,19vw,7.9rem)] sm:text-[clamp(4.8rem,12.2vw,11.8rem)] text-second-bg font-extrabold text-right leading-20 tracking-tight'
     >
       إصنع جسمًا أكثر{' '}
       <span className='word-slot relative inline-flex h-[93px] w-[7.6ch] sm:w-[7.1ch] mx-[0.2em] overflow-hidden align-baseline'>
