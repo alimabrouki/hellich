0850b0f154a240cd563c8be2e3cd83c357fb094f
alimab <alimabrouki9797@gmail.com>
2026-03-13 14:30:35 +0100

    add: replace head links with menu button when not at the top of the page

--- src/components/header/MenuButton.tsx
+++ src/components/header/MenuButton.tsx
@@ -1,15 +1,26 @@
 import type { animate, handleMenuProps, menuProps } from '../../types/types'
 
+type MenuButtonProps = menuProps &
+  handleMenuProps &
+  animate & {
+    showOnDesktop?: boolean
+  }
+
 function MenuButton ({
   handleMenuOpen,
   menuOpen,
-  animate
-}: menuProps & handleMenuProps & animate) {
+  animate,
+  showOnDesktop = false
+}: MenuButtonProps) {
+  const desktopVisibilityClass = showOnDesktop
+    ? 'hidden lg:inline-flex'
+    : 'inline-flex lg:hidden'
+
   return (
     <button
       type='button'
       onClick={() => handleMenuOpen(!menuOpen)}
-      className={`menu-button group relative lg:hidden py-2 px-3.5 bg-[#eaf9fb] mr-3 rounded-[3px]
+      className={`menu-button group relative ${desktopVisibilityClass} py-4 px-3.5 bg-[#eaf9fb] mr-3 rounded-[3px]
        
           ${menuOpen ? 'bg-black text-[#eaf9fb]' : 'text-main-bleu'}`}
       style={{ transitionDelay: animate ? '110ms' : '0ms' }}
@@ -28,7 +39,7 @@ function MenuButton ({
             bg-main-bleu transition-transform duration-300`}
       ></span>
 
-      <span className='relative inline-block -translate-x-2 translate-y-1.25 overflow-hidden align-bottom font-[650] leading-none'>
+      <span className='relative inline-block -translate-x-2 translate-y-1.25 lg:translate-y-1.5 overflow-hidden align-bottom font-[650] leading-none'>
         <span
           className={`block transition-all duration-300 ease-[cubic-bezier(0.76,0,0.24,1)]
               ${menuOpen ? '-translate-y-full blur-[10px] opacity-0' : ''}`}
