6ed84bea0570621c6f8814c48cc3fe4fd2e250c7
alimab <alimabrouki9797@gmail.com>
2026-03-29 15:07:40 +0100

    add: sliding words component

---
+++ src/components/SlidingWords.tsx
@@ -0,0 +1,35 @@
+import './SlidingWords.css'
+
+const words = [
+  'BURPEES',
+  'BENCH PRESS',
+  'SQUATS',
+  'PULL-UPS',
+  'BURPEES',
+  'PLANK',
+  'LUNGES',
+  'SQUATS',
+  'DEADLIFT',
+  'PUSH-UPS'
+]
+
+function SlidingWords () {
+  return (
+    <div className='sliding-words' dir='ltr' aria-hidden='true'>
+      <div className='sliding-words__track'>
+        {[0, 1].map(group => (
+          <div className='sliding-words__group' key={`group-${group}`}>
+            {words.map((word, index) => (
+              <span className='sliding-word' key={`${word}-${index}`}>
+                <span className='sliding-word__text'>{word}</span>
+                <span className='sliding-word__dot' />
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default SlidingWords
