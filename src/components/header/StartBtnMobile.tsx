47321adf0cc5a0c26f2fc8ed60137c5a55473059
alimab <alimabrouki9797@gmail.com>
2026-04-02 09:01:47 +0100

    add: inClick props to StartBtnMobile

--- src/components/header/StartBtnMobile.tsx
+++ src/components/header/StartBtnMobile.tsx
@@ -1,10 +1,15 @@
-import type { menuProps } from '../../types/types'
+﻿import type { menuProps } from '../../types/types'
 
 type StartBtnMobileProps = menuProps & {
   delayMs?: number
+  onClick?: () => void
 }
 
-function StartBtnMobile ({ menuOpen, delayMs = 0 }: StartBtnMobileProps) {
+function StartBtnMobile ({
+  menuOpen,
+  delayMs = 0,
+  onClick
+}: StartBtnMobileProps) {
   return (
     <a
       className={`text-head-btn bg-black self-center rounded-2xl px-9 py-2 flex items-center font-bold
@@ -16,6 +21,7 @@ function StartBtnMobile ({ menuOpen, delayMs = 0 }: StartBtnMobileProps) {
                   }`}
       href='#contact'
       style={{ transitionDelay: menuOpen ? `${delayMs}ms` : '0ms' }}
+      onClick={onClick}
     >
       إبدأ الآن
     </a>
