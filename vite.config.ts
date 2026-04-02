98150ad813612277bfb269aa384d3a7718fe7d41
alimab <alimabrouki9797@gmail.com>
2026-04-02 11:59:30 +0100

    fix: remove port

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,4 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ command }) => ({
-  plugins: [react(), tailwindcss()],
-  server: command === 'serve' ? {
-    proxy: {
-      "/api": "http://localhost:8787",
-    },
-  } : undefined,
-}))
+export default defineConfig({ plugins: [react(), tailwindcss()], server: { }, });
