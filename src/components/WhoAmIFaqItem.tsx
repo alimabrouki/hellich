6ab7ad5085b01b09a6355923ef6d818a2f00af1c
alimab <alimabrouki9797@gmail.com>
2026-04-02 17:27:34 +0100

    fix: lag when open faq answer

--- src/components/WhoAmIFaqItem.tsx
+++ src/components/WhoAmIFaqItem.tsx
@@ -1,4 +1,4 @@
-import { memo, useCallback, useRef, type CSSProperties, type SyntheticEvent, type TransitionEvent } from 'react'
+import { useCallback, useRef, type CSSProperties } from 'react'
 
 type FaqItem = {
   question: string
@@ -29,42 +29,6 @@ function WhoAmIFaqItem ({
     [index, onSetRef]
   )
 
-  const panelRef = useRef<HTMLDivElement | null>(null)
-  const handleToggle = useCallback(
-    (event: SyntheticEvent<HTMLDetailsElement>) => {
-      const panel = panelRef.current
-      if (!panel) return
-      const details = event.currentTarget
-      const height = panel.scrollHeight
-
-      if (details.open) {
-        panel.style.height = '0px'
-        requestAnimationFrame(() => {
-          panel.style.height = `${height}px`
-        })
-      } else {
-        panel.style.height = `${height}px`
-        requestAnimationFrame(() => {
-          panel.style.height = '0px'
-        })
-      }
-    },
-    []
-  )
-
-  const handleTransitionEnd = useCallback(
-    (event: TransitionEvent<HTMLDivElement>) => {
-      if (event.propertyName !== 'height') return
-      const panel = panelRef.current
-      if (!panel) return
-      const details = panel.closest('details')
-      if (details?.open) {
-        panel.style.height = 'auto'
-      }
-    },
-    []
-  )
-
   const revealStyle = {
     '--reveal-delay': `${index * revealDelayMs}ms`
   } as CSSProperties
@@ -75,7 +39,6 @@ function WhoAmIFaqItem ({
       style={revealStyle}
       data-faq-index={index}
       ref={handleRef}
-      onToggle={handleToggle}
     >
       <summary className='who-am-i-faq-button'>
         <span className='who-am-i-faq-text'>
@@ -86,16 +49,11 @@ function WhoAmIFaqItem ({
           <span className='who-am-i-faq-arrow' />
         </span>
       </summary>
-      <div
-        id={`faq-panel-${index}`}
-        className='who-am-i-faq-panel'
-        ref={panelRef}
-        onTransitionEnd={handleTransitionEnd}
-      >
+      <div id={`faq-panel-${index}`} className='who-am-i-faq-panel'>
         <p className='who-am-i-faq-answer'>{faq.answer}</p>
       </div>
     </details>
   )
 }
 
-export default memo(WhoAmIFaqItem)
+export default WhoAmIFaqItem
